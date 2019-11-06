188f82d15217a3bfcd4a614b72258ffd0091f51e
vjmarcus <vjmarcus1@gmail.com>
2019-11-06 20:29:14 +0300

    Add variables types

---
+++ src/Vars.kt
@@ -0,0 +1,21 @@
+
+
+public class Vars {
+
+
+    var byteNum : Byte = 127
+    var shortNum : Short = 234
+    var shortToInt : Int = shortNum.toInt() // short to int
+    var intNum : Int = 3234234
+    var intToLong : Long = intNum.toLong() // int to long
+    var intLong : Long = 23324234234234
+    var floatNum : Float = 23423432434F
+    var doubleNum : Double = 234234.34
+    var stringNum : String = "kotlin"
+    var charNum : Char = 'K'
+    var booleanNum : Boolean = true
+
+    //all variable save link to variable, all variable is object
+
+
+}
